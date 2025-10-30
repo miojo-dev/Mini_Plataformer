c343edc3953eca9e5fb4f0e6af9d19739f2e9c9e
João Vitor Hobus <69995779+miojo-dev@users.noreply.github.com>
2025-10-30 14:58:34 -0300

    feat/camera movement

--- Assets/Script/Managers/CameraManager.cs
+++ Assets/Script/Managers/CameraManager.cs
@@ -17,19 +17,16 @@ public class CameraManager : MonoBehaviour
     {
         float targetX = this.transform.position.x;
 
-        // Se o jogador ultrapassar o limite à direita (posicaoX do jogador > posicaoX da camera + threshold)
         if (player.position.x > targetX + rightThreshold)
         {
             targetX = player.position.x - rightThreshold;
         }
 
-        // Evita mover para trás igual no Mário
         if (targetX < _startX)
         {
             targetX = _startX;
         }
 
-        // Suaviza movimento
         var pos = transform.position;
         pos.x = Mathf.Lerp(
             transform.position.x,
