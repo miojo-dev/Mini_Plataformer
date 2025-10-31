5b1fc651416ebfc446e28217e45afa93b493a32e
João Vitor Hobus <69995779+miojo-dev@users.noreply.github.com>
2025-10-31 17:46:46 -0300

    fix/camera y axis movement

--- Assets/Script/Managers/CameraManager.cs
+++ Assets/Script/Managers/CameraManager.cs
@@ -4,18 +4,22 @@ public class CameraManager : MonoBehaviour
 {
     public Transform player;
     public float smooth;
-    public float rightThreshold; // distância à direita do centro antes da câmera mover
+    public float rightThreshold;
+    public float topThreshold;
 
     private float _startX;
+    private float _startY;
 
     private void Start()
     {
-        _startX = transform.position.x;
+        _startX = this.transform.position.x;
+        _startY = this.transform.position.y;
     }
 
     private void LateUpdate()
     {
         float targetX = this.transform.position.x;
+        float targetY = player.position.y - topThreshold;
 
         if (player.position.x > targetX + rightThreshold)
         {
@@ -27,12 +31,24 @@ public class CameraManager : MonoBehaviour
             targetX = _startX;
         }
 
+        if (targetY < _startY)
+        {
+            targetY = _startY;
+        }
+
         var pos = transform.position;
+
         pos.x = Mathf.Lerp(
             transform.position.x,
             targetX,
             Time.deltaTime * smooth
         );
+        pos.y = Mathf.Lerp(
+            transform.position.y,
+            targetY,
+            Time.deltaTime * smooth
+        );
+
         transform.position = pos;
     }
 }
